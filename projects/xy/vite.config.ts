0be38178e77c4771d6365800bc3e90d3b00689a6
Johann Haslinger <johann.haslinger@centigrade.de>
2026-06-09 16:00:06 +0200

    Generate projects/xy/vite.config.ts

---
+++ projects/xy/vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
+
+export default defineConfig({
+  base: "/component-platform/xy/",
+  plugins: [
+    react(),
+    federation({
+      name: "xy",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./src/components/Button/Button.tsx",
+        "./Input": "./src/components/Input/Input.tsx"
+},
+      shared: {
+        react: {
+          singleton: true,
+        },
+        "react-dom": {
+          singleton: true,
+        },
+        "react/": {
+          singleton: true,
+        },
+        "react-dom/": {
+          singleton: true,
+        },
+      },
+    }),
+  ],
+  build: {
+    target: "chrome89",
+  },
+});
